90874b1f909d2694dfaf4e812e0a744df5d8a7c8
xtadeushx <alekscherkasov13@gmail.com>
2022-08-14 15:10:17 +0300

    fill: add redux/toolkit store

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { ThemeProvider } from './provider/ThemeProvider';
 import './styles/index.scss';
+import { store } from './store/store';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <BrowserRouter>
-   <React.StrictMode>
-   <ThemeProvider>
-     <App />
-   </ThemeProvider>
-   
-  </React.StrictMode>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>,
 );
-
