1d488f36fd078f4c32a3e7bb9255484c24db3dee
xtadeushx <alekscherkasov13@gmail.com>
2022-08-15 12:10:07 +0300

    Fill: add custom hooks useSelector and useDispatch

--- src/services/WeatherService.ts
+++ src/services/WeatherService.ts
@@ -1,11 +1,12 @@
 import axios, { AxiosResponse } from 'axios';
+import api from '../axios';
 import { Weather } from '../types/types';
 
-const URL = 'https://api.openweathermap.org/data/3.0/';
-const API_KEY = process.env.REACT_APP_API_KEY;
+// const URL = 'https://api.openweathermap.org/data/3.0/';
+// const API_KEY = process.env.REACT_APP_API_KEY;
 
 export class WeatherSErvice {
   static getCurrentWeather(city: string):Promise<AxiosResponse<Weather>> {
-    return axios.get(`${URL}weather?q=${city}&appid=${API_KEY}`);
+    return api.get(`/weather?q=${city}`);
   }
 }
